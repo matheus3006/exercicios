1bc75b0c5a6e383d747bd46015f4288779bb3d2b
matheus3006 <matheussouza18@live.com>
2022-08-08 21:38:08 -0300

    feat: second ex

---
+++ pages/basicos/jsx2.js
@@ -0,0 +1,14 @@
+export default function jsx2 (){
+    const conteudo = (
+    <div>
+        <h1>
+             JSX2 #02 
+        </h1>
+    </div>
+    )
+    
+    return (
+        conteudo
+
+    )
+}
