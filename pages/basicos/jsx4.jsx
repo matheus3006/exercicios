2e146929e50b36e402258024b74c1d5a95375c93
matheus3006 <matheussouza18@live.com>
2022-08-08 21:46:38 -0300

    feat: fourth ex

---
+++ pages/basicos/jsx4.jsx
@@ -0,0 +1,11 @@
+export default function jsx4() {
+    const subtitulo = "Estou no JS"    
+    
+    return(
+            <div>
+                <h1>Intraçao JS e JSX</h1>
+                <h2>{subtitulo}</h2>
+            </div>
+        )
+
+}
