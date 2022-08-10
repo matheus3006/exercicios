8cff549218eb2d014bcae59f2fc2cd0c6b749472
matheus3006 <matheussouza18@live.com>
2022-08-10 08:09:18 -0300

    feat: ex frag

---
+++ pages/basicos/fragment.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function fragment(){
+    return (
+        <React.fragment>
+            <h1>Titulo</h1>
+            <h2>subtitulo</h2>
+        </React.fragment>
+    )
+}
