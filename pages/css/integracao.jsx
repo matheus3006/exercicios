2fd5d3eb6774aeed29432989c59f5488c3b554f5
matheus3006 <matheussouza18@live.com>
2022-08-17 07:59:37 -0300

    feat: integracao css

---
+++ pages/css/integracao.jsx
@@ -0,0 +1,11 @@
+import styles from './integracao1.css'
+
+export default function integracao(){
+    return(
+        <div>
+            <div className={styles.vermelha}>Texto 01#</div>
+            <div className={styles.azul}>Texto 02#</div>
+            <div className={styles.branca}>Texto 03#</div>
+        </div>
+    )
+}
